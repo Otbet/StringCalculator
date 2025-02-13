dfc93d8f6a8231a66eb8c96986b309b53e376bc4
Durga Prasad Pennada <durgaprasad.pennada@outlook.com>
2025-02-14 02:52:58 +0530

    Starting from Initial Code

--- StringCalculator.App/StringCalculator.cs
+++ StringCalculator.App/StringCalculator.cs
@@ -11,39 +11,7 @@ namespace StringCalculator.App
     {
         public int Add(string numbers)
         {
-            if (string.IsNullOrEmpty(numbers))
-                return 0;
-
-            var delimiters = new List<string> { ",", "\n" };
-            var match = Regex.Match(numbers, "^//(\\[.*?\\])\\n");
-
-            if (match.Success)
-            {
-                numbers = numbers.Substring(match.Length);
-                var customDelimiters = Regex.Matches(match.Groups[1].Value, "\\[(.*?)\\]")
-                    .Cast<Match>()
-                    .Select(m => m.Groups[1].Value)
-                    .ToList();
-                delimiters.AddRange(customDelimiters);
-            }
-            else if (numbers.StartsWith("//"))
-            {
-                delimiters.Add(numbers[2].ToString());
-                numbers = numbers.Substring(4);
-            }
-
-            var numberList = numbers.Split(delimiters.ToArray(), StringSplitOptions.None)
-                .Select(int.Parse)
-                .ToList();
-
-            var negatives = numberList.Where(n => n < 0).ToList();
-            if (negatives.Any())
-            {
-                throw new ArgumentException("negatives not allowed: " + string.Join(", ", negatives));
-            }
-
-            int result = numberList.Where(n => n <= 1000).Sum();
-            return result;
+            return 0;
         }
 
     }
